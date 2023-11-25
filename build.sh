a9b68174fefc70e50d62b468ec857af7f83add37
Joseph Kanichai <kjosephsubash@gmail.com>
2023-11-25 18:26:26 +0100

    Add graphscope driver to build script

--- build.sh
+++ build.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-cargo build --release
-mkdir -p bin
-cp target/release/benchmark-runner bin/bench
+cargo build --release || exit -1
+mkdir -p bin || exit -1
+cp target/release/benchmark-runner bin/bench || exit -1
+echo Runner built!
+
+drivers=(graphscope)
+
+for i in "${drivers[@]}"
+do
+    pushd drivers/$i
+    docker build -t $i . || exit -1
+    echo $i image built!
+done
