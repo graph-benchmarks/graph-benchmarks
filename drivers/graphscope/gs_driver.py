90380a656c1bd36e6f28cb7f17c1c21fb783a4df
AlfredDaimari <alfred_daimari@yahoo.com>
2023-11-28 10:47:12 +0100

    fix - time

--- drivers/graphscope/gs_driver.py
+++ drivers/graphscope/gs_driver.py
@@ -5,43 +5,10 @@ import psycopg.sql as sql
 import yaml
 import time
 from graphscope.framework import loader
-
-# functional arguments position for the program
-# config_file_path id1 id2 algorithm dataset
-
-config_yml = sys.argv[1]
-_id = int(sys.argv[2])
-_id2 = int(sys.argv[3])
-algo = sys.argv[4]
-dataset = sys.argv[5]
-
-with open(config_yml, 'r') as yml_file:
-    configs = yaml.safe_load(yml_file)
-
-gs_host = configs["graphscope"]["host"]
-gs_port = configs["graphscope"]["port"]
-
-try:
-    sess = gs.session(addr=f"{gs_host}:{gs_port}")
-except:
-    print("Error: could not connect to graphscope cluster")
-    exit()
-g = sess.g()
-
-pg_host = configs["postgres"]["host"]
-pg_db = configs["postgres"]["db"]
-pg_port = configs["postgres"]["port"]
-user_ps = configs["postgres"]["ps"]
-pg_user = configs["postgres"]["user"]
-
-try:
-    conn = psycopg.connect(f"postgresql://{pg_user}:{user_ps}@{pg_host}:{pg_port}/{pg_db}")
-except:
-    print("Error: could not connect to postgresql database")
-    exit()
+from graphscope.framework.graph import Graph, GraphDAGNode
 
 # check if table exists on postgres
-def check_table()->None:
+def check_table(conn: psycopg.Connection)->None:
     cur = conn.cursor()
     query = sql.SQL("SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'gn_test') AS table_existence")
     ret = cur.execute(query)
@@ -53,7 +20,7 @@ def check_table()->None:
     conn.commit()       
     cur.close()
 
-def log_metrics_sql(log_id:int, type_:str, time:float)->None:
+def log_metrics_sql(conn: psycopg.Connection, log_id:int, algo:str, dataset:str, type_:str, time:float)->None:
     columns = ["id", "algo", "dataset", "type", "time"]
     cur = conn.cursor()
     query = sql.SQL("INSERT INTO gn_test ({}) VALUES ({})").format(
@@ -64,22 +31,21 @@ def log_metrics_sql(log_id:int, type_:str, time:float)->None:
     conn.commit()
     cur.close()
 
-# load vertex, edge files
-def load_data(g: gs.Graph): 
-    vertex_file = configs["dataset"][dataset]["vertex"]
-    edge_file = configs["dataset"][dataset]["edges"] 
-    
+def load_data(g: Graph | GraphDAGNode, vertex_file:str, edge_file:str)->int:
+    """
+    Returns loading time
+    """
     v = loader.Loader(f"file://{vertex_file}", header_row=False)
     e = loader.Loader(f"file://{edge_file}", header_row=False)
 
-    start_time = time.time()
+    start_time = time.clock_gettime_ns(time.CLOCK_MONOTONIC)
     g = g.add_vertices(v, 'vertex')
     g = g.add_edges(e, 'edges')
-    end_time = time.time()
+    end_time = time.clock_gettime_ns(time.CLOCK_MONOTONIC)
 
     duration = end_time - start_time
-    log_metrics_sql(_id, "loading", duration)
-
+    return duration
+    
 
 def bfs(g):
     gs.bfs(g)
@@ -104,16 +70,65 @@ def lcc(g):
 def sssp(g):
     gs.sssp(g)
 
-check_table()
-load_data(g)
-func_d = {'bfs': bfs, 'pr':pr, 'wcc':wcc, 'cdlp':cdlp, 'lcc':lcc, 'sssp':sssp}
+def main():
+    # functional arguments position for the program
+    # config_file_path id1 id2 algorithm dataset log_file
+    
+    config_yml = sys.argv[1]
+    load_id = int(sys.argv[2])
+    compute_id = int(sys.argv[3])
+    algo = sys.argv[4]
+    dataset = sys.argv[5]
+    log_file = sys.argv[6]
 
-start_time = time.time()
-func_d[algo](g)
-end_time = time.time()
+    lf = open(log_file, "w+")
 
-duration = end_time - start_time
-log_metrics_sql(_id2, "runtime", duration)
+    with open(config_yml, 'r') as yml_file:
+        configs = yaml.safe_load(yml_file)
 
-sess.close()
-conn.close()
+    gs_host = configs["graphscope"]["host"]
+    gs_port = configs["graphscope"]["port"]
+
+    pg_host = configs["postgres"]["host"]
+    pg_db = configs["postgres"]["db"]
+    pg_port = configs["postgres"]["port"]
+    user_ps = configs["postgres"]["ps"]
+    pg_user = configs["postgres"]["user"]
+
+    vertex_file = configs["dataset"][dataset]["vertex"]
+    edge_file = configs["dataset"][dataset]["edges"] 
+    
+    try:
+        conn = psycopg.connect(f"postgresql://{pg_user}:{user_ps}@{pg_host}:{pg_port}/{pg_db}")
+    except:
+        lf.write("Error: could not connect to postgresql database\n")
+        lf.close()
+        quit(1)
+
+    try:
+        sess = gs.session(addr=f"{gs_host}:{gs_port}")
+    except:
+        lf.write("Error: could not connect to graphscope cluster\n")
+        lf.close()
+        conn.close()    
+        quit(1)
+    
+    g = sess.g()
+    check_table(conn)
+    duration = load_data(g, vertex_file, edge_file)
+    log_metrics_sql(conn, load_id, algo, dataset, "loading", duration)
+
+    func_d = {'bfs': bfs, 'pr':pr, 'wcc':wcc, 'cdlp':cdlp, 'lcc':lcc, 'sssp':sssp}
+
+    start_time = time.clock_gettime_ns(time.CLOCK_MONOTONIC)
+    func_d[algo](g)
+    end_time = time.clock_gettime_ns(time.CLOCK_MONOTONIC)
+
+    duration = end_time - start_time
+    log_metrics_sql(conn, compute_id, algo, dataset, "runtime", duration)
+
+    lf.close()
+    sess.close()
+    conn.close()
+
+main()
