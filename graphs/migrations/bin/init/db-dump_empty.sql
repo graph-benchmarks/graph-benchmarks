cc595af25441566909e60dd6a95cd489790417cb
Lars Meijer <lars.m@tryfe.nl>
2023-11-29 17:16:27 +0100

    Added dummy data sql script 4 algos, 4 datasets

--- graphs/migrations/bin/init/db-dump_empty.sql
+++ graphs/migrations/bin/init/db-dump_empty.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 16.1
 -- Dumped by pg_dump version 16.1 (Homebrew)
 
--- Started on 2023-11-29 17:09:29 CET
+-- Started on 2023-11-23 19:50:25 CET
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -18,10 +18,14 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+DROP DATABASE postgres;
 --
 -- TOC entry 3350 (class 1262 OID 5)
 -- Name: postgres; Type: DATABASE; Schema: -; Owner: user
 --
+
+CREATE DATABASE postgres WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDER = libc LOCALE = 'en_US.utf8';
+
 
 ALTER DATABASE postgres OWNER TO "user";
 
@@ -54,6 +58,7 @@ COMMENT ON DATABASE postgres IS 'default administrative connection database';
 
 CREATE SCHEMA IF NOT EXISTS public;
 
+
 ALTER SCHEMA public OWNER TO pg_database_owner;
 
 --
@@ -70,7 +75,7 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- TOC entry 215 (class 1259 OID 16384)
+-- TOC entry 215 (class 1259 OID 24576)
 -- Name: driver_logging; Type: TABLE; Schema: public; Owner: user
 --
 
@@ -89,31 +94,15 @@ CREATE TABLE public.driver_logging (
 ALTER TABLE public.driver_logging OWNER TO "user";
 
 --
--- TOC entry 3344 (class 0 OID 16384)
+-- TOC entry 3344 (class 0 OID 24576)
 -- Dependencies: 215
 -- Data for Name: driver_logging; Type: TABLE DATA; Schema: public; Owner: user
 --
 
-INSERT INTO public.driver_logging VALUES (1, 'algo1', 'dataset1', 2, 10, 1024, 'runtime', 5000);
-INSERT INTO public.driver_logging VALUES (2, 'algo1', 'dataset2', 2, 10, 1024, 'runtime', 4000);
-INSERT INTO public.driver_logging VALUES (3, 'algo1', 'dataset3', 2, 10, 1024, 'runtime', 4500);
-INSERT INTO public.driver_logging VALUES (4, 'algo1', 'dataset4', 2, 10, 1024, 'runtime', 2500);
-INSERT INTO public.driver_logging VALUES (5, 'algo2', 'dataset1', 2, 10, 1024, 'runtime', 4800);
-INSERT INTO public.driver_logging VALUES (6, 'algo2', 'dataset2', 2, 10, 1024, 'runtime', 4000);
-INSERT INTO public.driver_logging VALUES (7, 'algo2', 'dataset3', 2, 10, 1024, 'runtime', 5200);
-INSERT INTO public.driver_logging VALUES (8, 'algo2', 'dataset4', 2, 10, 1024, 'runtime', 3000);
-INSERT INTO public.driver_logging VALUES (9, 'algo3', 'dataset1', 2, 10, 1024, 'runtime', 5400);
-INSERT INTO public.driver_logging VALUES (10, 'algo3', 'dataset2', 2, 10, 1024, 'runtime', 4500);
-INSERT INTO public.driver_logging VALUES (11, 'algo3', 'dataset3', 2, 10, 1024, 'runtime', 4800);
-INSERT INTO public.driver_logging VALUES (12, 'algo3', 'dataset4', 2, 10, 1024, 'runtime', 3500);
-INSERT INTO public.driver_logging VALUES (13, 'algo4', 'dataset1', 2, 10, 1024, 'runtime', 4500);
-INSERT INTO public.driver_logging VALUES (14, 'algo4', 'dataset2', 2, 10, 1024, 'runtime', 3800);
-INSERT INTO public.driver_logging VALUES (15, 'algo4', 'dataset3', 2, 10, 1024, 'runtime', 4200);
-INSERT INTO public.driver_logging VALUES (16, 'algo4', 'dataset4', 2, 10, 1024, 'runtime', 2400);
 
 
 --
--- TOC entry 3200 (class 2606 OID 16390)
+-- TOC entry 3200 (class 2606 OID 24582)
 -- Name: driver_logging structure_driver_logging_pk; Type: CONSTRAINT; Schema: public; Owner: user
 --
 
@@ -121,7 +110,7 @@ ALTER TABLE ONLY public.driver_logging
     ADD CONSTRAINT structure_driver_logging_pk PRIMARY KEY (id);
 
 
--- Completed on 2023-11-29 17:09:29 CET
+-- Completed on 2023-11-23 19:50:25 CET
 
 --
 -- PostgreSQL database dump complete
