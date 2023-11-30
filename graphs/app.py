4fc08ffd0e791095a3c2c1f62b81510de9e857ab
Lars Meijer <lars.m@tryfe.nl>
2023-11-30 11:07:04 +0100

    Code split generate_histrograms()

--- graphs/app.py
+++ graphs/app.py
@@ -1,9 +1,8 @@
 import psycopg2
 import os
 import time
-from datetime import datetime
-import matplotlib.pyplot as plt
-import numpy as np
+
+from generate_histograms import generate_histograms
 
 # DB connection & credential variables.
 db_host = os.environ.get("POSTGRES_HOST")
@@ -21,96 +20,49 @@ output_directory = "/app/results/"
 select_log_id = os.environ.get("SELECT_LOG_ID")
 print(f"Selecting data of log id: {select_log_id}")
 
-try:
-    # Connect to the PostgreSQL database
-    connection = psycopg2.connect(
-        host=db_host,
-        port=db_port,
-        database=db_name,
-        user=db_user,
-        password=db_password
-    )
-
-    # Connect to the databse.
-    cursor = connection.cursor()
-    print("connected to db...")
-
-    # Query the data from the database.
-    query = "SELECT * FROM driver_logging"
-    cursor.execute(query)
-    
-    # Fetch the results and iterate over them. Group first per logged algorithm
-    # and then per dataset for the specific algorithm.
-    data_groups = {}
-    algorithms = set()
-    datasets = set()
-    rows = cursor.fetchall() 
-    for row in rows:
-        log_id, algo, dataset, cpu, workers, mem_size, log_type, time = row
-
-        algorithms.add(algo)
-        datasets.add(dataset)
-        if algo not in data_groups:
-            data_groups[algo] = {}
-        if dataset not in data_groups[algo]:
-            data_groups[algo][dataset] = []
-        data_groups[algo][dataset].append(time)
-
-    # Sort the algorithms and datasets for consistent order.
-    algorithms = sorted(algorithms)
-    datasets = sorted(datasets)
-
-    # Create a figure and axis
-    fig, ax = plt.subplots()
-    
-    # Some white space between the multiple groups of algorithms.
-    bar_width = 0.2
-
-    # Create a list of x positions for each group of bars.
-    x = np.arange(len(algorithms))
-
-    # Create a bar for each dataset within each algorithm.
-    for i, dataset in enumerate(datasets):
-        times = [data_groups[algo][dataset][0] if dataset in data_groups[algo] else 0 for algo in algorithms]
-        ax.barh(x + i * bar_width, times, height=bar_width, label=dataset)
-
-    # Plot design.
-    ax.set_xlabel("Time")
-    ax.set_ylabel("Algorithms")
-    ax.set_title(
-        "Execution Time for Datasets per Algorithm", 
-        fontweight="bold"
-    )
-    ax.set_yticks(x + bar_width * (len(datasets) - 1) / 2)
-    ax.set_yticklabels(algorithms)
-    ax.legend(title="Datasets")
-    plt.xticks(rotation=45, ha="right")
-    plt.tight_layout()
-    
-    # Writing the graph to a file.
-    timestamp_str = datetime.now().strftime("%Y%m%d%H%M")
-    output_filename = os.path.join(
-        output_directory, 
-        f"result-{select_log_id}-{timestamp_str}.png"
+def main():
+    try:
+        # Connect to the PostgreSQL database
+        connection = psycopg2.connect(
+            host=db_host,
+            port=db_port,
+            database=db_name,
+            user=db_user,
+            password=db_password
         )
-    plt.savefig(output_filename)
-    print(f"Graph saved to {output_filename}")
-    
 
-except psycopg2.Error as error:
-    print("Error connecting to PostgreSQL:", error)
-    
-    # Common reason for the connection to fail is that the db isn't able
-    # to receive new connections yet. 
-    # Therefore sleep before restarted by Docker.
-    print("Go to sleep before exitting...")
-    time.sleep(2)
-    
-    exit()
+        # Connect to the databse.
+        cursor = connection.cursor()
+        print("connected to db...")
 
-finally:
-    # Close the cursor and connection
-    if cursor:
-        cursor.close()
-    if connection:
-        connection.close()
+        # Query the data from the database.
+        query = "SELECT * FROM driver_logging"
+        cursor.execute(query)
+        
+        # Fetch the results and iterate over them. Group first per logged algorithm
+        # and then per dataset for the specific algorithm.
+        rows = cursor.fetchall() 
+        generate_histograms(rows, output_directory, select_log_id)
+        
+
+    except psycopg2.Error as error:
+        print("Error connecting to PostgreSQL:", error)
+        
+        # Common reason for the connection to fail is that the db isn't able
+        # to receive new connections yet. 
+        # Therefore sleep before restarted by Docker.
+        print("Go to sleep before exitting...")
+        time.sleep(2)
+        
+        exit()
+
+    finally:
+        # Close the cursor and connection
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
+
+
+if __name__ == "__main__":
+    main()
