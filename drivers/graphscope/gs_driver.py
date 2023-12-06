2edc43ea62a955248378b5c451b4aecc56ab4b60
AlfredDaimari <alfred_daimari@yahoo.com>
2023-12-06 22:20:10 +0100

    fix: weights

--- drivers/graphscope/gs_driver.py
+++ drivers/graphscope/gs_driver.py
@@ -4,7 +4,7 @@ import psycopg
 import psycopg.sql as sql
 import yaml
 import time
-import requests
+import pandas as pd
 from graphscope.framework.graph import Graph, GraphDAGNode
 
 # check if table exists on postgres
@@ -14,7 +14,7 @@ def check_table(conn: psycopg.Connection)->None:
     ret = cur.execute(query)
     
     if not ret.fetchone()[0]:
-        query = sql.SQL("CREATE TABLE gn_test(id INTEGER, algo VARCHAR(256), dataset VARCHAR(256), type VARCHAR(256), time INTEGER, vertex INTEGER, edge INTEGER, nodes INTEGER)")  
+        query = sql.SQL("CREATE TABLE gn_test(id INTEGER, algo VARCHAR(256), dataset VARCHAR(256), type VARCHAR(256), time INTEGER, vertex INTEGER, edge INTEGER)")  
         cur.execute(query)
  
     conn.commit()       
@@ -29,15 +29,15 @@ def graph_vertex_edge_count(sess:gs.Session, g:Graph | GraphDAGNode):
     return tot_vertex, tot_edges
 
 
-def log_metrics_sql(conn: psycopg.Connection, log_id:int, algo:str, dataset:str, type_:str, time:float, vertex:int, edge:int, nodes:int)->None:
-    columns = ["id", "algo", "dataset", "type", "time", "vertex", "edge", "nodes"]
+def log_metrics_sql(conn: psycopg.Connection, log_id:int, algo:str, dataset:str, type_:str, time:float, vertex:int, edge:int)->None:
+    columns = ["id", "algo", "dataset", "type", "time", "vertex", "edge"]
     cur = conn.cursor()
     query = sql.SQL("INSERT INTO gn_test ({}) VALUES ({})").format(
             sql.SQL(', ').join(map(sql.Identifier, columns)),
             sql.SQL(', ').join(sql.Placeholder() * len(columns)))
 
     time_ms = time // 1000000
-    cur.execute(query, (log_id, algo, dataset, type_, time_ms, vertex, edge, nodes))
+    cur.execute(query, (log_id, algo, dataset, type_, time_ms, vertex, edge))
     conn.commit()
     cur.close()
 
@@ -50,14 +50,14 @@ def load_data(config, sess:gs.Session, vertex_file:str, edge_file:str):
     g = sess.g(directed=config["dataset"]["directed"])
 
     start_time = time.clock_gettime_ns(time.CLOCK_MONOTONIC)
-    #df_v = pd.read_csv(vertex_file, header=None, names=["vertex"])
+    df_v = pd.read_csv(vertex_file, header=None, names=["vertex"])
     
-    #if config["dataset"]["weights"]:
-    #    df_e = pd.read_csv(edge_file, header=None, names=["src","dst"], sep=" ")
-    #else:
-    #    df_e = pd.read_csv(edge_file, header=None, names=["src", "dst", "weights"], sep=" ")
+    if not config["dataset"]["weights"]:
+        df_e = pd.read_csv(edge_file, header=None, names=["src","dst"], sep=" ")
+    else:
+        df_e = pd.read_csv(edge_file, header=None, names=["src", "dst", "weights"], sep=" ")
 
-    g = g.add_vertices(vertex_file, vid_field="vertex").add_edges(edge_file, src_field="src", dst_field="dst")
+    g = g.add_vertices(df_v).add_edges(df_e)
     end_time = time.clock_gettime_ns(time.CLOCK_MONOTONIC)
     duration = end_time - start_time
 
@@ -124,7 +124,6 @@ def main():
     #sql params
     id_ = int(config["config"]["id"])
     algo = config["config"]["algo"]
-    nodes = config["config"]["nodes"]
 
     log_file = config["config"]["log_file"]
     lf = open(log_file, "w+")
@@ -163,16 +162,14 @@ def main():
     #vertex = graph_vertex_count(g)
     #edge = graph_edge_count(g)
 
-    log_metrics_sql(conn, id_, algo, dataset, "loading", duration, vertex, edge, nodes)
+    log_metrics_sql(conn, id_, algo, dataset, "loading", duration, vertex, edge)
 
     func_d = {'bfs': bfs, 'pr':pr, 'wcc':wcc, 'cdlp':cdlp, 'lcc':lcc, 'sssp':sssp}
 
-    requests.post('http://notifier:8080/starting')
     dur = func_d[algo](config, g) 
-    requests.post('http://notifier:8080/stopping')
 
     if dur > 0:
-        log_metrics_sql(conn, id_, algo, dataset, "runtime", dur, vertex, edge, nodes)
+        log_metrics_sql(conn, id_, algo, dataset, "runtime", dur, vertex, edge)
 
     lf.close()
     sess.close()
