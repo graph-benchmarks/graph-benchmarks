6d565679107af1f3973c5366a7089251f3cb5169
AlfredDaimari <alfred_daimari@yahoo.com>
2023-12-12 00:06:10 +0100

    fix: status->state

--- drivers/arango/pregel_result.js
+++ drivers/arango/pregel_result.js
@@ -1,5 +1,5 @@
 
 var pregel = require("@arangodb/pregel")
 const execution = "!!!----!!!"  // replace this with the execution time
-console.log(`\n!===${pregel.status(execution)["status"]}===!\n`)
-console.log(`\n!===#${pregel.status(execution)["running_time"]}===#!\n`)
+console.log(`\n!===${pregel.status(execution)["state"]}===!\n`)
+console.log(`\n!===#${pregel.status(execution)["totalRuntime"]}===#!\n`)
